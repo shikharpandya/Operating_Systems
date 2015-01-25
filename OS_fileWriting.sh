6bf6cc6b38002cc1959055af56e7851ebf80b6a2
shikharpandya <shikhar.pandya@iet.ahduni.edu.in>
2015-01-25 22:48:15 +0530

    OS lab assigment 2 submission (Updated)
    This commit conatins 2 files, one of which is the output files for all
    the commands that were run on the terminal, while the other is a .sh
    file (bash) containing the commands.

---
+++ OS_fileWriting.sh
@@ -0,0 +1,35 @@
+date>output.txt
+factor 65 >> output.txt
+diff SpellCheckThis CheckForDiff >> output.txt
+grep -i 'is' SpellCheckThis >> output.txt
+du >> output.txt
+tty >> output.txt
+who -H >> output.txt
+df >> output.txt
+whereis CreatedViaTerminal >> output.txt
+iw >> output.txt
+last >> output.txt
+sort SpellCheckThis >> output.txt
+ifconfig >> output.txt
+uname -a >> output.txt
+seq 5 20 >> output.txt
+seq 10 2 50 >> output.txt
+ps -u >> output.txt
+sync >> output.txt
+who -b >> output.txt
+free >> output.txt
+more SpellCheckThis >> output.txt
+less SpellCheckThis >> output.txt
+head SpellCheckThis >> output.txt
+dmesg >> output.txt
+jobs >> output.txt
+lsusb >> output.txt
+last >> output.txt
+lastb >> output.txt
+
+top >> output.txt
+unexpand SpellCheckThis >> output.txt
+rev SpellCheckThis >> output.txt
+spell SpellCheckThis >> output.txt
+
+
